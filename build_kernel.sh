b4fc707993634815d988d078cdc89c0b5e479511
Satyendra Singh <satyendra.xv@gmail.com>
2026-06-10 14:00:26 +0000

    Update build_kernel.sh script to build and pack kernel

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
-export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export clang-triple = android/prebuilts/clang/host/linux-x86/clang-4639204/bin/aarch64-linux-gnu-
-export ANDROID_MAJOR_VERSION=s
-export PLATFORM_VERSION=12
+CLANG_BIN="${PWD}/toolchain/clang/bin"
+GCC_BIN="${PWD}/toolchain/gcc/bin"
 
-make exynos9610-m31snsxx_defconfig
-make -j64
+# check for toolchain dir
+if [ ! -d "$CLANG_BIN" ] || [ ! -d "$GCC_BIN" ]; then
+  echo "[BUILD]: Toolchain not found.."
+  echo "[BUILD]: Run ./resolv_toolchain_linux.sh to resolve toolchain"
+  exit 1
+else
+  echo "[BUILD]: Toolchain found.."
+fi
+
+
+function K_MAKE() {
+  local options=$*
+  
+  echo
+  echo "[K_MAKE]: $options"
+  sleep 1
+  
+  ANDROID_MAJOR_VERSION=s \
+  PLATFORM_VERSION=12 \
+  make O=out ARCH=arm64 CONFIG_SECTION_MISMATCH_WARN_ONLY=y CONFIG_DEBUG_SECTION_MISMATCH=y $options
+}
+
+K_MAKE mrproper
+K_MAKE clean
+K_MAKE exynos9610-m31snsxx_defconfig
+K_MAKE -j$(nproc --all)
+
+# AIK pack
+echo "[BUILD]: AIK pack.."
+
+cp -rv ./out/arch/arm64/boot/Image ./AnyKernel3/Image
+
+cd AnyKernel3 || exit 1
+TIMESTAMP=$(date +"%Y-%m-%dT%H-%M-%S")
+zip -r "../SM-M317F_kernel_${TIMESTAMP}.zip" * -x .git README.md '*placeholder'
+cd .. || exit 1
